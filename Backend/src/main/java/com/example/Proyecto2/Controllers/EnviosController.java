e7ea466c59093dc9f190dd9715f1ba382df1c93a
YasminMiranda03 <3946136940101@ingenieria.usac.edu.gt>
2025-12-28 09:51:14 -0600

    modificaciones al backend

--- Backend/src/main/java/com/example/Proyecto2/Controllers/EnviosController.java
+++ Backend/src/main/java/com/example/Proyecto2/Controllers/EnviosController.java
@@ -7,6 +7,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
+
 @RequestMapping("/api/envios")
 public class EnviosController {
 	 private final ServiceEnviosINT serviceEnvios;
